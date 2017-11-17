ddfa9ba89ed8b1d77914e6122e85f9b4de02ff73
Salvador Repasa <jun.repasa@gmail.com>
2017-11-18 10:24:25 +1300

    ovewrite repo version

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,7 @@
 
-var chalk =require('chalk');
+
+//originally written in ES5
+// var chalk =require('chalk');
+import chalk from  'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
